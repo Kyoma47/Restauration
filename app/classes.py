d5297d347853dc57cfe06cbb3f51e1f60caeffee
Kyoma47 <mediane.jawad@gmail.com>
2020-06-10 04:01:28 +0000

    client terminé

--- app/classes.py
+++ app/classes.py
@@ -28,11 +28,12 @@ class Produit :
         self.image = image
         self.description = random.choice(phrases) #description
 
-class Employer:
-    def __init__(self, nom, prenom, image, statut, desctiption,
+class Employe:
+    def __init__(self, prenom, nom, image, statut, desctiption="",
         facebook  = "https//facebook.com",
         twitter   = "https//twitter.com",
-        instagram = "https//instagram.com"
+        instagram = "https//instagram.com",
+        youtube   = "https://youtube.com"
     ):
         self.nom    = nom
         self.prenom = prenom
@@ -43,3 +44,42 @@ class Employer:
         self.facebook  = facebook
         self.twitter   = twitter
         self.instagram = instagram
+        self.youtube   = youtube
+
+desserts = [
+    Produit("Crepes aux mirtilles", "06.jpg", 12.25),
+    Produit("Gateaux chinois", "08.jpg", 4),
+    Produit("Gateaux a la creme", "gateau.jpeg", 12.25),
+    Produit("Macarons", "macarons.jpeg", 7.5)
+]
+pizza = [
+    Produit("Margarita", "02.jpg", 20.3),
+    Produit("Pizza 4 saisons", "4saisons.jpeg", 32.3),
+    Produit("Mini Pizza", "mini-pizza.jpeg", 12),
+    Produit("Pepperoni", "pepperoni.jpeg", 53.3)
+]
+plats = [
+    Produit("Plat poulet", "03.jpg", 20.3),
+    Produit("Pattes Carbonara", "04.jpg", 32.3),
+    Produit("Boulettes viande hachee", "05.jpg", 12)
+]
+salades = [
+    Produit("Salade Potager", "01.jpg", 13.75),
+    Produit("Salade Nicoise", "07.jpg", 12.75),
+    Produit("Salade au fromage", "salade-fromage.jpeg", 20.5),
+    Produit("Salade a la mayonaise", "salade-mayo.jpeg", 12),
+    Produit("Salade variee", "salade-variee.jpeg", 13.5)
+]
+categories = [
+    Categorie("Pizza"  , "pizza"   , pizza),
+    Categorie("Salade" , "salades" , salades),
+    Categorie("Plat"   , "plats"   , plats),
+    Categorie("Dessert", "desserts", desserts)
+]
+employes = [
+    Employe("Jennifer", "Soft", "about-01.jpg", "Fondateur CEO", youtube=None),
+    Employe("Daisy",   "Walker", "about-02.jpg", "Chef d'executuion", twitter=None, youtube=None),
+    Employe("Florence", "Nelson", "about-03.jpg", "Manager de cuisine", twitter=None),
+    Employe("Valentina", "Martin", "about-04.jpg", "Directeur Culinaire")
+
+]
--- app/views/client_views.py
+++ app/views/client_views.py
@@ -1,45 +1,15 @@
 from app import app
 from flask import Flask, request, render_template
 
-from app.classes import Produit, Categorie
+from app.classes import *
 
 @app.route('/')
 def accueil():
-    desserts = [
-        Produit("Crepes aux mirtilles", "06.jpg", 12.25),
-        Produit("Gateaux chinois", "08.jpg", 4),
-        Produit("Gateaux a la creme", "gateau.jpeg", 12.25),
-        Produit("Macarons", "macarons.jpeg", 7.5)
-    ]
-    pizza = [
-        Produit("Margarita", "02.jpg", 20.3),
-        Produit("Pizza 4 saisons", "4saisons.jpeg", 32.3),
-        Produit("Mini Pizza", "mini-pizza.jpeg", 12),
-        Produit("Pepperoni", "pepperoni.jpeg", 53.3)
-    ]
-    plats = [
-        Produit("Plat poulet", "03.jpg", 20.3),
-        Produit("Pattes Carbonara", "04.jpg", 32.3),
-        Produit("Boulettes viande hachee", "05.jpg", 12)
-    ]
-    salades = [
-        Produit("Salade Potager", "01.jpg", 13.75),
-        Produit("Salade Nicoise", "07.jpg", 12.75),
-        Produit("Salade au fromage", "salade-fromage.jpeg", 20.5),
-        Produit("Salade a la mayonaise", "salade-mayo.jpeg", 12),
-        Produit("Salade variee", "salade-variee.jpeg", 13.5)
-    ]
-    categories = [
-        Categorie("Pizza"  , "pizza"   , pizza),
-        Categorie("Salade" , "salades" , salades),
-        Categorie("Plat"   , "plats"   , plats),
-        Categorie("Dessert", "desserts", desserts)
-    ]
     return render_template("index.html", categories=categories)
 
 @app.route('/a-propos')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", employes=employes)
 
 @app.route('/contact')
 def contact():
