7f584515be39acd602e2293406366dbc30f60a03
Kyoma47 <mediane.jawad@gmail.com>
2020-06-29 18:28:20 +0000

    plus de fonctions

--- SQL_Server/fonctions_scalaires.sql
+++ SQL_Server/fonctions_scalaires.sql
@@ -10,3 +10,17 @@ BEGIN
   ) AS T
   RETURN  @prixMenu;
 END
+
+
+ALTER PROCEDURE dbo.ps_select_plats_du_menu (@IdMenu INT)
+AS
+BEGIN
+	IF dbo.udfNombreDePlats(@IdMenu) > 1
+	BEGIN
+		--IdMenu, IdPlat, Qt, Remise, Description, prix, livrable, Image
+		SELECT *, (Qt * (Prix - Prix*remise/100) ) AS PrixQtRemise
+		FROM TPLATS_MENUS,TPLATS
+		WHERE
+    IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
+	END
+END
--- SQL_Server/porcedures_stockees.sql
+++ SQL_Server/porcedures_stockees.sql
@@ -1,9 +1,20 @@
-CREATE PROCEDURE ps_select_menus
+ALTER PROCEDURE ps_select_menus
 AS
 BEGIN
-  SELECT * FROM TMENUS
+  SELECT *, dbo.udfPrixMenu(IdMenu) AS PrixMenu FROM TMENUS
   WHERE (
 	   SELECT Count(*) FROM TPLATS_MENUS
 	   WHERE TMENUS.IdMenu = TPLATS_MENUS.IdMenu
 	) > 1
+END
+
+
+ALTER PROCEDURE ps_select_plats_du_menu (@IdMenu INT)
+AS
+BEGIN
+  --IdMenu, IdPlat, Qt, Remise, Description, prix, livrable, Image
+  SELECT *, (Qt * (Prix - Prix*remise/100) ) AS PrixQtRemise
+  FROM TPLATS_MENUS,TPLATS
+  WHERE
+    IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
 END
