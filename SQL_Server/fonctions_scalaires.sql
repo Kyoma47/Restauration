b4dfda29bc6a505271774964d1bd7530a65d193e
Kyoma47 <mediane.jawad@gmail.com>
2020-06-29 23:46:53 +0000

    fonctions : udfNombreDePlats, udfPrixMenu procedure : ps_select_menus, ps_select_plats_du_menu

--- SQL_Server/fonctions_scalaires.sql
+++ SQL_Server/fonctions_scalaires.sql
@@ -1,26 +1,39 @@
+IF EXISTS (
+    SELECT * FROM sysobjects WHERE id = object_id(N'udfNombreDePlats')
+    AND xtype IN (N'FN', N'IF', N'TF')
+)
+    DROP FUNCTION udfNombreDePlats
+GO
+
+CREATE FUNCTION dbo.udfNombreDePlats(@IdMenu INTEGER)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @nombrePlats AS INT;
+  SELECT  @nombrePlats = COUNT(*) FROM TPLATS_MENUS,TPLATS
+  WHERE IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
+  RETURN  @nombrePlats;
+END
+GO
+--------------------------------------------------------------------------
+
+IF EXISTS (
+    SELECT * FROM sysobjects WHERE id = object_id(N'udfPrixMenu')
+    AND xtype IN (N'FN', N'IF', N'TF')
+)
+    DROP FUNCTION udfPrixMenu
+GO
+
 CREATE FUNCTION dbo.udfPrixMenu(@IdMenu INTEGER)
 RETURNS INT
 AS
 BEGIN
-  DECLARE  @prixMenu  AS INT;
-  SELECT @prixMenu = SUM(Total) FROM (
+  DECLARE @prixMenu AS INT;
+  SELECT  @prixMenu = SUM(Total) FROM (
 	   SELECT (Qt * (Prix - Prix*remise/100) ) AS Total
 	   FROM TPLATS_MENUS,TPLATS
 	   WHERE IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
   ) AS T
   RETURN  @prixMenu;
 END
-
-
-ALTER PROCEDURE dbo.ps_select_plats_du_menu (@IdMenu INT)
-AS
-BEGIN
-	IF dbo.udfNombreDePlats(@IdMenu) > 1
-	BEGIN
-		--IdMenu, IdPlat, Qt, Remise, Description, prix, livrable, Image
-		SELECT *, (Qt * (Prix - Prix*remise/100) ) AS PrixQtRemise
-		FROM TPLATS_MENUS,TPLATS
-		WHERE
-    IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
-	END
-END
+GO
--- SQL_Server/porcedures_stockees.sql
+++ SQL_Server/porcedures_stockees.sql
@@ -1,20 +1,40 @@
-ALTER PROCEDURE ps_select_menus
+IF EXISTS (
+  SELECT type_desc, type FROM sys.procedures WITH(NOLOCK)
+  WHERE NAME = 'ps_select_menus' AND type = 'P'
+)
+  DROP PROCEDURE dbo.ps_select_menus
+GO
+
+CREATE PROCEDURE ps_select_menus
 AS
 BEGIN
   SELECT *, dbo.udfPrixMenu(IdMenu) AS PrixMenu FROM TMENUS
   WHERE (
-	   SELECT Count(*) FROM TPLATS_MENUS
-	   WHERE TMENUS.IdMenu = TPLATS_MENUS.IdMenu
+	   SELECT COUNT(*) FROM TPLATS_MENUS
+	   WHERE TMENUS.Id Menu = TPLATS_MENUS.IdMenu
 	) > 1
 END
+GO
+
+-------------------------------------------------------------------------
+
+IF EXISTS (
+  SELECT type_desc, type FROM sys.procedures WITH(NOLOCK)
+  WHERE NAME = 'ps_select_plats_du_menu' AND type = 'P'
+)
+  DROP PROCEDURE dbo.ps_select_plats_du_menu
+GO
 
 
-ALTER PROCEDURE ps_select_plats_du_menu (@IdMenu INT)
+CREATE PROCEDURE dbo.ps_select_plats_du_menu (@IdMenu INT)
 AS
 BEGIN
-  --IdMenu, IdPlat, Qt, Remise, Description, prix, livrable, Image
-  SELECT *, (Qt * (Prix - Prix*remise/100) ) AS PrixQtRemise
-  FROM TPLATS_MENUS,TPLATS
-  WHERE
+	IF dbo.udfNombreDePlats(@IdMenu) > 1
+	BEGIN
+		--IdMenu, IdPlat, Qt, Remise, Description, prix, livrable, Image
+		SELECT *, (Qt * (Prix - Prix*remise/100) ) AS PrixQtRemise
+		FROM TPLATS_MENUS,TPLATS
+		WHERE
     IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
+	END
 END
