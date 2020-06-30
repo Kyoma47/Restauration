9dd92b497b9a269b7899dce0dc20c6368f2d0044
Kyoma47 <mediane.jawad@gmail.com>
2020-06-30 22:21:12 +0000

    fusion fonctions_procedures.sql

--- SQL_Server/fonctions_porcedures.sql
+++ SQL_Server/fonctions_porcedures.sql
@@ -1,4 +1,46 @@
 IF EXISTS (
+    SELECT * FROM sysobjects WHERE id = object_id(N'udfNombreDePlats')
+    AND xtype IN (N'FN', N'IF', N'TF')
+)
+    DROP FUNCTION udfNombreDePlats
+GO
+
+CREATE FUNCTION dbo.udfNombreDePlats(@IdMenu INTEGER)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @nombrePlats AS INT;
+  SELECT  @nombrePlats = COUNT(*) FROM TPLATS_MENUS,TPLATS
+  WHERE IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
+  RETURN  @nombrePlats;
+END
+GO
+--------------------------------------------------------------------------
+
+IF EXISTS (
+    SELECT * FROM sysobjects WHERE id = object_id(N'udfPrixMenu')
+    AND xtype IN (N'FN', N'IF', N'TF')
+)
+    DROP FUNCTION udfPrixMenu
+GO
+
+CREATE FUNCTION dbo.udfPrixMenu(@IdMenu INTEGER)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @prixMenu AS INT;
+  SELECT  @prixMenu = SUM(Total) FROM (
+	   SELECT (Qt * (Prix - Prix*remise/100) ) AS Total
+	   FROM TPLATS_MENUS,TPLATS
+	   WHERE IdMenu = @IdMenu AND TPLATS.IdPlat = TPLATS_MENUS.IdPlat
+  ) AS T
+  RETURN  @prixMenu;
+END
+GO
+---------------------------------------------------------------------------
+-- Fonctions(haut) /  Procedures(bas)
+---------------------------------------------------------------------------
+IF EXISTS (
   SELECT type_desc, type FROM sys.procedures WITH(NOLOCK)
   WHERE NAME = 'ps_select_menus' AND type = 'P'
 )
